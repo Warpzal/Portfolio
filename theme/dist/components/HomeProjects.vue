3297cea71ce70c0a699d3465ee2ff9678e9e9c15
Zalonics <anthony.dev.9898@gmail.com>
2022-12-08 10:45:11 -0800

    Fixed: CSS for projects, and wrote new post

--- theme/dist/components/HomeProjects.vue
+++ theme/dist/components/HomeProjects.vue
@@ -298,7 +298,7 @@ export default {
 <style scoped>
 .grid {
     display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
     gap: 20px;
 }
 .category-link {
